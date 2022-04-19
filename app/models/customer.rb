496356d055f98a5b6e7706dc6c77b8dc0b8845f3
takahashi1106 <tk2696274@gmail.com>
2022-04-19 10:19:35 +0000

    [Start]

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,4 @@
-class Public::Customer < ApplicationRecord
+class Customer < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
