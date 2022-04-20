bcf26f4596a74cf74859de4148b9f59d686219b0
takahashi1106 <tk2696274@gmail.com>
2022-04-20 08:12:46 +0000

    [katsuki]

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,13 @@
 class Item < ApplicationRecord
   has_one_attached:image
+
+  has_many :cart_items
+  has_many :ordered_items
+
+
+  #税込み価格
+ def with_tax_price
+    (price * 1.1).floor
+ end
+
 end
