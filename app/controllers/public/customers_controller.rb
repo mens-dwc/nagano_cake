c7281ef5bf4d506fcf38fde3a55aa583cfee7f23
Hiro1006 <hota1006@gmail.com>
2022-04-22 06:05:29 +0000

    Top編集

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -4,7 +4,7 @@ class Public::CustomersController < ApplicationController
   end
 
   def edit
-    @customer = Address.find(params[:id])
+    @customer = Customer.find(params[:id])
   end
 
   def update
