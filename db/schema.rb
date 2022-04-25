61e776a6097b08b8151e69ac65c62b13e4d6b20d
EC2 Default User <ec2-user@ip-172-31-90-29.ec2.internal>
2022-04-25 07:50:42 +0000

    [add]

--- db/schema.rb
+++ db/schema.rb
@@ -91,7 +91,6 @@ ActiveRecord::Schema.define(version: 2022_04_23_050737) do
 
   create_table "genres", force: :cascade do |t|
     t.string "name"
-    t.boolean "is_genres_status", default: false, null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
@@ -127,6 +126,18 @@ ActiveRecord::Schema.define(version: 2022_04_23_050737) do
     t.integer "postage", default: 800
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
