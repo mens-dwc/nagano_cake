e1e9fac7e9f02573106e8a79263054fd75d10e63
takahashi1106 <tk2696274@gmail.com>
2022-04-26 07:39:51 +0000

    [一度]

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,11 +1,10 @@
 class Public::ItemsController < ApplicationController
-  #ログインユーザーのみproduct#indexは閲覧可
 
   def index #items_path
     @items=Item.page(params[:page]).per(4)
     @itemss =Item.where(is_status: 'false')
     @genres = Genre.all
-    #Denreのshow  genre_path
+    #Genreのshow  genre_path
   end
 
   def show #item_path(item.id)
