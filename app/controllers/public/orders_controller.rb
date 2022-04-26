4c641dd0676cc67ed87a4afb864069a42b7bc417
Hiro1006 <hota1006@gmail.com>
2022-04-26 08:43:39 +0000

    完了

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -6,17 +6,7 @@ class Public::OrdersController < ApplicationController
   end
 
   def comfirm #注文確認
-    params[:order][:payment_type] = params[:order][:payment_type].to_i #payment_methodの数値に変換
     @order = Order.new(order_params)
-    
-    
-        #支払い方法のセッション情報
-    if params[:payment_select] == "0"
-      session[:user][:payment_type] = 0
-    elsif params[:payment_select] == "1"
-      session[:user][:payment_type] = 1
-    end
-
 
     if params[:order][:address_option] == "0" #cutomerの登録時の住所
       @order.postal_code = current_customer.postal_code
