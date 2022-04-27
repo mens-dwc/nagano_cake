7fbd531ade82dcec752f0cb38f858276057b3899
Hiro1006 <hota1006@gmail.com>
2022-04-27 08:49:02 +0000

    テスト完了

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -8,7 +8,7 @@ before_action :authenticate_admin!
   def create #admin_items_path
      @item =Item.new(item_params)
     if @item.save
-      redirect_to admin_items_path
+      redirect_to admin_item_path(@item.id)
     else
       render :new
     end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
 
    def after_sign_in_path_for(resource)
       if customer_signed_in?
-        about_path
+        root_path
       else
         admin_root_path
       end
@@ -22,7 +22,11 @@ class ApplicationController < ActionController::Base
 
 
   def after_sign_out_path_for(resource)
+    if customer_signed_in?
     root_path
+    else
+    admin_session_path
+    end
   end
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :kana_first_name, :kana_last_name, :email, :address, :postal_code, :tel_number])
